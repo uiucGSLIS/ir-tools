f60fa1d38ad28ec0a49553baa9e66722c84fc520
craig-willis <willis8@illinois.edu>
2014-07-28 09:35:31 -0500

    Changed to use IndexWrapperFactory

--- src/main/java/edu/gslis/docscoring/support/IndexBackedCollectionStats.java
+++ src/main/java/edu/gslis/docscoring/support/IndexBackedCollectionStats.java
@@ -2,14 +2,14 @@ package edu.gslis.docscoring.support;
 
 
 import edu.gslis.indexes.IndexWrapper;
-import edu.gslis.indexes.IndexWrapperIndriImpl;
+import edu.gslis.indexes.IndexWrapperFactory;
 
 
 public class IndexBackedCollectionStats extends CollectionStats {	
 	protected IndexWrapper index;
 	
 	public void setStatSource(String statSource) {
-		this.index = new IndexWrapperIndriImpl(statSource);
+		this.index = IndexWrapperFactory.getIndexWrapper(statSource);
 		tokCount = index.termCount();
 		docCount = index.docCount();
 		termTypeCount = index.termTypeCount();
