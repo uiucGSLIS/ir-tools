7a47ef7a30352c5bda97528d11e632ea5b8f6092
craig-willis <willis8@illinois.edu>
2014-07-31 10:29:10 -0500

    LuceneBuildIndex now supports multiple threads/shards.

--- src/main/java/edu/gslis/lucene/main/LuceneBuildIndex.java
+++ src/main/java/edu/gslis/lucene/main/LuceneBuildIndex.java
@@ -7,7 +7,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -63,121 +67,54 @@ public class LuceneBuildIndex {
      */
     public void buildIndex() throws Exception {
 
-        String indexPath = config.getIndexPath();        
-        Directory dir = FSDirectory.open(new File(indexPath));
-
-        // Initialize the analyzer
-        StopwordAnalyzerBase defaultAnalyzer;
-        String stopwordsPath = config.getStopwords();
-        String analyzerClass = config.getAnalyzer();
-        if (!StringUtils.isEmpty(analyzerClass))
-        {
-            @SuppressWarnings("rawtypes")
-            Class analyzerCls = loader.loadClass(analyzerClass);
-    
-            if (!StringUtils.isEmpty(stopwordsPath))
-            {
-                @SuppressWarnings({ "rawtypes", "unchecked" })
-                java.lang.reflect.Constructor analyzerConst = analyzerCls.getConstructor(Version.class, Reader.class);
-                defaultAnalyzer = (StopwordAnalyzerBase)analyzerConst.newInstance(Indexer.VERSION, new FileReader(stopwordsPath));            
-            } else {
-                @SuppressWarnings({ "rawtypes", "unchecked" })
-                java.lang.reflect.Constructor analyzerConst = analyzerCls.getConstructor(Version.class);
-                defaultAnalyzer = (StopwordAnalyzerBase)analyzerConst.newInstance(Indexer.VERSION);            
-            }
-        } else {
-            defaultAnalyzer = new StandardAnalyzer(Indexer.VERSION, new CharArraySet(Indexer.VERSION, 0, true));
-        }
-        
-        // Assumes LM similarity, but can be changed via config file
-        Similarity similarity = new LMDirichletSimilarity();
-        String similarityClass = config.getSimilarity();
-        if (!StringUtils.isEmpty(similarityClass))
-            similarity = (Similarity)loader.loadClass(similarityClass).newInstance();
-        
-        // Setup any per-field analyzers.
-        Map<String, Analyzer> perFieldAnalyzers = new HashMap<String, Analyzer>();
-        Set<FieldConfig> fields = config.getFields();
-        for (FieldConfig field: fields) {
-            String fieldAnalyzerClass = field.getAnalyzer();            
-            String fieldType = field.getType();
-
-            if (!StringUtils.isEmpty(fieldAnalyzerClass)) {
-                // Use per-field analyzer, if present
-                @SuppressWarnings("rawtypes")
-                Class fieldAnalyzerCls = loader.loadClass(fieldAnalyzerClass);
-                Analyzer fieldAnalyzer = (Analyzer)fieldAnalyzerCls.newInstance();
-                perFieldAnalyzers.put(field.getName(), fieldAnalyzer);
-            }
-            else if (!StringUtils.isEmpty(fieldType)&& fieldType.equals(FieldConfig.TYPE_ID)) {
-                // If the field type is ID, default to KeywordAnalyzer.
-                Analyzer fieldAnalyzer = new KeywordAnalyzer();
-                perFieldAnalyzers.put(field.getName(), fieldAnalyzer);
-            }            
-        }
-        
-        Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
-        IndexWriterConfig iwc = new IndexWriterConfig(Indexer.VERSION, analyzer);
-        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-        iwc.setRAMBufferSizeMB(256.0);
-        iwc.setSimilarity(similarity);
-        
-        IndexWriter writer = new IndexWriter(dir, iwc);
         CorpusConfig corpusConfig = config.getCorpus();
+
+        int numShards = corpusConfig.getShards();
+    
+        String filter = corpusConfig.getFilter();
         String corpusPath = corpusConfig.getPath();
-        String corpusType = corpusConfig.getType();
+        File corpusFile = new File(corpusPath);
+        
+        // Top level list of files to be processed
+        List<File> files = new ArrayList<File>();
+        if (!StringUtils.isEmpty(filter)) {
+            FileFilter fileFilter = new WildcardFileFilter(filter);
+            File[] filtered = corpusFile.listFiles(fileFilter);
+            for (File file: filtered)
+                files.add(file);
+        } else if (corpusFile.isDirectory()){
+            File[] fs = corpusFile.listFiles();
+            for (File file: fs)
+                files.add(file);
+        } else
+            files.add(corpusFile);
+        
+        Collections.sort(files);
+        
+        int shardSize = files.size() / numShards;
+        List<List<File>> shards = new LinkedList<List<File>>(); 
+        for (int i = 0; i < files.size(); i += shardSize) {
+            shards.add(files.subList(i, i + Math.min(shardSize, files.size() - i)));
+        }
 
-        try
-        {
-            Indexer indexer;
-            if (corpusType.equals(Indexer.FORMAT_WIKI)) {      
-                indexer = new WikiTextIndexer();
-            } else if (corpusType.equals(Indexer.FORMAT_TRECTEXT)){ 
-                indexer = new TrecTextIndexer();
-            } else if (corpusType.equals(Indexer.FORMAT_TIKA)) {
-                indexer = new TikaIndexer();                
-            } else if (corpusType.equals(Indexer.FORMAT_STREAMCORPUS)) {
-                indexer = new StreamCorpusIndexer();                
-            } else {
-                throw new Exception("Unsupported corpus type/format.");                
-            }
-            
-            String filter = corpusConfig.getFilter();
-            File corpusFile = new File(corpusPath);
-            long count = 0;
-            if (!StringUtils.isEmpty(filter)) {
-                FileFilter fileFilter = new WildcardFileFilter(filter);
-                File[] files = corpusFile.listFiles(fileFilter);
-                for (File file: files) {
-                    count += indexer.buildIndex(writer,  fields, file);
-                }                    
-            } else {
-                count += indexer.buildIndex(writer,  fields, new File(corpusPath));
-            }
-            System.out.println("Indexed " + count + " files");
+        List<Thread> threads = new LinkedList<Thread>();
+        int i= 0;
+        for (List<File> shardFiles: shards) {
+            Runnable task = new IndexerThread(String.valueOf(i), shardFiles);
+            Thread worker = new Thread(task);
+            worker.setName(String.valueOf(i));
+            worker.start();
+            threads.add(worker);
+            i++;
+        }        
+        
 
-            writeIndexMetadata(indexPath, config);
-        } catch (Exception e) {
-            System.out.println("Fatal: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            writer.close();
-        }   
+        
+        for (Thread thread: threads) {
+            thread.join();
+        }
     }
     
-    public void writeIndexMetadata(String indexPath, IndexConfig config) 
-            throws IOException {
-        // Need to hold on to a few pieces of information
-        FileWriter metadataWriter = new FileWriter(indexPath + File.separator + "index.metadata");
-        
-        String analyzer = config.getAnalyzer();
-        if (!StringUtils.isEmpty(analyzer))
-            metadataWriter.write("analyzer=" + analyzer);
-        String similarity = config.getSimilarity();
-        if (!StringUtils.isEmpty(similarity))
-            metadataWriter.write("similarity=" + similarity);
-        metadataWriter.close();
-    }
 
     public static void main(String[] args) throws Exception {
         
@@ -197,5 +134,135 @@ public class LuceneBuildIndex {
 
         LuceneBuildIndex builder = new LuceneBuildIndex(config);
         builder.buildIndex();
+    }
+    
+    private class IndexerThread implements Runnable {
+        
+        List<File> files;
+        String id;
+        
+        public IndexerThread(String id, List<File> files)
+        {
+            this.id = id;
+            this.files = files;
+        }
+         
+        public void run() {
+            
+            System.out.println("Starting thread " + id);
+            long count = 0;
+            try
+            {
+                String indexPath = config.getIndexPath() + File.separator + "shard" + id;        
+                Directory dir = FSDirectory.open(new File(indexPath));
+    
+                // Initialize the analyzer
+                StopwordAnalyzerBase defaultAnalyzer;
+                String stopwordsPath = config.getStopwords();
+                String analyzerClass = config.getAnalyzer();
+                if (!StringUtils.isEmpty(analyzerClass))
+                {
+                    @SuppressWarnings("rawtypes")
+                    Class analyzerCls = loader.loadClass(analyzerClass);
+            
+                    if (!StringUtils.isEmpty(stopwordsPath))
+                    {
+                        @SuppressWarnings({ "rawtypes", "unchecked" })
+                        java.lang.reflect.Constructor analyzerConst = analyzerCls.getConstructor(Version.class, Reader.class);
+                        defaultAnalyzer = (StopwordAnalyzerBase)analyzerConst.newInstance(Indexer.VERSION, new FileReader(stopwordsPath));            
+                    } else {
+                        @SuppressWarnings({ "rawtypes", "unchecked" })
+                        java.lang.reflect.Constructor analyzerConst = analyzerCls.getConstructor(Version.class);
+                        defaultAnalyzer = (StopwordAnalyzerBase)analyzerConst.newInstance(Indexer.VERSION);            
+                    }
+                } else {
+                    defaultAnalyzer = new StandardAnalyzer(Indexer.VERSION, new CharArraySet(Indexer.VERSION, 0, true));
+                }
+                
+                // Assumes LM similarity, but can be changed via config file
+                Similarity similarity = new LMDirichletSimilarity();
+                String similarityClass = config.getSimilarity();
+                if (!StringUtils.isEmpty(similarityClass))
+                    similarity = (Similarity)loader.loadClass(similarityClass).newInstance();
+                
+                // Setup any per-field analyzers.
+                Map<String, Analyzer> perFieldAnalyzers = new HashMap<String, Analyzer>();
+                Set<FieldConfig> fields = config.getFields();
+                for (FieldConfig field: fields) {
+                    String fieldAnalyzerClass = field.getAnalyzer();            
+                    String fieldType = field.getType();
+    
+                    if (!StringUtils.isEmpty(fieldAnalyzerClass)) {
+                        // Use per-field analyzer, if present
+                        @SuppressWarnings("rawtypes")
+                        Class fieldAnalyzerCls = loader.loadClass(fieldAnalyzerClass);
+                        Analyzer fieldAnalyzer = (Analyzer)fieldAnalyzerCls.newInstance();
+                        perFieldAnalyzers.put(field.getName(), fieldAnalyzer);
+                    }
+                    else if (!StringUtils.isEmpty(fieldType)&& fieldType.equals(FieldConfig.TYPE_ID)) {
+                        // If the field type is ID, default to KeywordAnalyzer.
+                        Analyzer fieldAnalyzer = new KeywordAnalyzer();
+                        perFieldAnalyzers.put(field.getName(), fieldAnalyzer);
+                    }            
+                }
+                
+                Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
+                IndexWriterConfig iwc = new IndexWriterConfig(Indexer.VERSION, analyzer);
+                iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+                iwc.setRAMBufferSizeMB(256.0);
+                iwc.setSimilarity(similarity);
+                            
+                IndexWriter writer = new IndexWriter(dir, iwc);
+                CorpusConfig corpusConfig = config.getCorpus();
+                String corpusType = corpusConfig.getType();
+                    
+                try
+                {
+                    Indexer indexer;
+                    if (corpusType.equals(Indexer.FORMAT_WIKI)) {      
+                        indexer = new WikiTextIndexer();
+                    } else if (corpusType.equals(Indexer.FORMAT_TRECTEXT)){ 
+                        indexer = new TrecTextIndexer();
+                    } else if (corpusType.equals(Indexer.FORMAT_TIKA)) {
+                        indexer = new TikaIndexer();                
+                    } else if (corpusType.equals(Indexer.FORMAT_STREAMCORPUS)) {
+                        indexer = new StreamCorpusIndexer();                
+                    } else {
+                        throw new Exception("Unsupported corpus type/format.");                
+                    }
+        
+                    for (File file: files) {
+                        count += indexer.buildIndex(writer,  fields, file);                
+                    }
+                    System.out.println("Indexed " + count + " files");
+                
+    
+                    writeIndexMetadata(indexPath, config);
+                } catch (Exception e) {
+                    System.out.println("Fatal: " + e.getMessage());
+                    e.printStackTrace();
+                } finally {
+                    writer.close();
+                }   
+            } catch (Exception e) {
+                System.out.println("Fatal: " + e.getMessage());
+                e.printStackTrace();                
+            }
+        }
+        
+        public void writeIndexMetadata(String indexPath, IndexConfig config) 
+                throws IOException {
+            // Need to hold on to a few pieces of information
+            FileWriter metadataWriter = new FileWriter(indexPath + File.separator + "index.metadata");
+            
+            String analyzer = config.getAnalyzer();
+            if (!StringUtils.isEmpty(analyzer))
+                metadataWriter.write("analyzer=" + analyzer);
+            String similarity = config.getSimilarity();
+            if (!StringUtils.isEmpty(similarity))
+                metadataWriter.write("similarity=" + similarity);
+            metadataWriter.close();
+        }
+
     }
 }
