618b042218270699b50cd122b402c24433271d65
craig-willis <willis8@illinois.edu>
2014-08-08 09:58:03 -0500

    Added timestamp field name

--- src/main/java/edu/gslis/filtering/session/FilterSession.java
+++ src/main/java/edu/gslis/filtering/session/FilterSession.java
@@ -5,6 +5,7 @@ import edu.gslis.searchhits.SearchHits;
 public interface FilterSession {
 	public static final String NAME_OF_EMIT_STATUS_FIELD = "emit";
 	public static final String NAME_OF_CONSTRAINT_FIELD  = "constraint";
+    public static final String NAME_OF_TIMESTAMP_FIELD  = "timestamp";
 	
 	public void train();
 	
