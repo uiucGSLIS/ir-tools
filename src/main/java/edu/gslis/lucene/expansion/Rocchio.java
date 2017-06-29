8eb871caf5e4c0fe921b6a7a761debb2872858fd
Craig Willis <willis8@illinois.edu>
2017-06-29 14:48:15 -0500

    Minor restructuring/renaming, added stopper

--- src/main/java/edu/gslis/lucene/expansion/Rocchio.java
+++ src/main/java/edu/gslis/lucene/expansion/Rocchio.java
@@ -3,15 +3,12 @@ package edu.gslis.lucene.expansion;
 
 import java.io.IOException;
 
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-
 import edu.gslis.indexes.IndexWrapper;
 import edu.gslis.queries.GQuery;
 import edu.gslis.searchhits.SearchHit;
 import edu.gslis.searchhits.SearchHits;
 import edu.gslis.textrepresentation.FeatureVector;
+import edu.gslis.utils.Stopper;
 
 public class Rocchio {
 	
@@ -19,6 +16,8 @@ public class Rocchio {
 	private double beta;
 	private double k1;
 	private double b;
+	
+	private Stopper stoplist = new Stopper();
 	
 	/**
 	 * Default parameter values taken from:
@@ -39,39 +38,42 @@ public class Rocchio {
 		this.b = b;
 	}
 	
+	public void setStopper(Stopper stoplist) {
+		this.stoplist = stoplist;
+	}
+	
 	public void expandQuery(IndexWrapper index, GQuery query, int fbDocs, int fbTerms) throws IOException {
 		
 		SearchHits hits = index.runQuery(query, fbDocs);
 		
-		FeatureVector summedTermVec = new FeatureVector(null);
+		FeatureVector feedbackVec = new FeatureVector(stoplist);
 		
 		for (SearchHit hit: hits.hits()) {
 			// Get the document tokens and add to the doc vector
-			FeatureVector docVec = index.getDocVector(hit.getDocID(), null);
+			FeatureVector docVec = index.getDocVector(hit.getDocID(), stoplist);
 			
-			// Compute the BM25 weights
-			computeBM25Weights(index, docVec, summedTermVec);
+			// Compute the BM25 weights and add to the feedbackVector
+			computeBM25Weights(index, docVec, feedbackVec);
 		}
 		
 		// Multiply the summed term vector by beta / |Dr|
-		FeatureVector relDocTermVec = new FeatureVector(null);
-		for (String term : summedTermVec.getFeatures()) {
-			relDocTermVec.addTerm(term, summedTermVec.getFeatureWeight(term) * beta / fbDocs);
+		FeatureVector relDocTermVec = new FeatureVector(stoplist);
+		for (String term : feedbackVec.getFeatures()) {
+			relDocTermVec.addTerm(term, feedbackVec.getFeatureWeight(term) * beta / fbDocs);
 		}
 		
 		// Create a query vector and scale by alpha
-		FeatureVector rawQueryVec = new FeatureVector(null);
-		parseText(query.getText(), rawQueryVec);
+		FeatureVector origQueryVec = query.getFeatureVector();
 		
-		FeatureVector summedQueryVec = new FeatureVector(null);
-		computeBM25Weights(index, rawQueryVec, summedQueryVec);
+		FeatureVector weightedQueryVec = new FeatureVector(stoplist);
+		computeBM25Weights(index, origQueryVec, weightedQueryVec);
 		
-		FeatureVector queryTermVec = new FeatureVector(null);
-		for (String term : rawQueryVec.getFeatures()) {
-			queryTermVec.addTerm(term, summedQueryVec.getFeatureWeight(term) * alpha);
+		FeatureVector queryTermVec = new FeatureVector(stoplist);
+		for (String term : origQueryVec.getFeatures()) {
+			queryTermVec.addTerm(term, weightedQueryVec.getFeatureWeight(term) * alpha);
 		}
 		
-		// Combine query and rel doc vectors
+		// Combine query and feedback vectors
 		for (String term : queryTermVec.getFeatures()) {
 			relDocTermVec.addTerm(term, queryTermVec.getFeatureWeight(term));
 		}
@@ -79,24 +81,7 @@ public class Rocchio {
 		// Get top terms
 		relDocTermVec.clip(fbTerms);
 		
-		StringBuffer expandedQuery = new StringBuffer();
-		for (String term : relDocTermVec.getFeatures()) {
-			expandedQuery.append(term + "^" + relDocTermVec.getFeatureWeight(term) + " ");
-		}
-		
-		query.setText(expandedQuery.toString());
-	}
-	
-	private void parseText(String text, FeatureVector vector) throws IOException {
-		StandardAnalyzer analyzer = new StandardAnalyzer();
-		TokenStream tokenStream = analyzer.tokenStream(null, text);
-		CharTermAttribute tokens = tokenStream.addAttribute(CharTermAttribute.class);
-		tokenStream.reset();
-		while (tokenStream.incrementToken()) {
-			String token = tokens.toString();
-			vector.addTerm(token);
-		}
-		analyzer.close();	
+		query.setFeatureVector(relDocTermVec);
 	}
 	
 	private void computeBM25Weights(IndexWrapper index, FeatureVector docVec, FeatureVector summedTermVec) throws IOException {
@@ -109,6 +94,7 @@ public class Rocchio {
 			double tf = docVec.getFeatureWeight(term);
 			
 			double weight = (idf * k1 * tf) / (tf + k1 * (1 - b + b * docVec.getLength() / avgDocLen));
+					
 			summedTermVec.addTerm(term, weight);
 		}
 	}
