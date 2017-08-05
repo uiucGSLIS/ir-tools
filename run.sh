4655a28ada621b60e5a4377346a4078402e8e861
Craig Willis <willis8@illinois.edu>
2017-08-05 09:59:14 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-# $1 : class.to.run
-# $2 : config file
-java -Xmx4g -Djava.library.path="/usr/local/lib" -cp target/ir-utils-0.0.1-SNAPSHOT.jar:./target/classes/* $1 $2
+java -cp "target/ir-utils-0.2.0-SNAPSHOT.jar:target/lib/*" $@
